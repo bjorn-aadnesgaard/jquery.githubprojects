7cf82b82563987b0c9fb53d191d43b49b4428515
Bjorn Aadnesgaard <bjorn@conekted.com>
2013-01-19 00:20:45 -0500

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,122 @@
+﻿# jquery.githubprojects
+
+A simple jQuery plugin for rendering a list of Github projects for a specified user.
+
+## Installation
+
+	<script src="/path/to/jquery.githubprojects.js"></script>
+
+## Usage
+
+Place a div with the ID __github-projects__ in your markup.
+
+	<div id="github-projects"></div>
+
+Call the plugin
+
+	$('#github-projects').githubprojects({
+		username: 'bjorn-aadnesgaard'
+	});
+
+## Rendered HTML Sample with Default Options
+
+	<div class="project-github">
+		<div class="item">
+			<div class="title">
+				<h4>
+					<i class="icon"></i>
+					<a href="https://github.com/bjorn-aadnesgaard/jquery.githubprojects">
+						jquery.githubprojects
+					</a>
+				</h4>
+			</div>
+			<div class="description">
+				Renders html markup with a list of Github projects for a specified user.
+			</div>
+			<a href="https://github.com/bjorn-aadnesgaard/jquery.githubprojects" class="btn">
+				View Project
+			</a>
+		</div>
+	</div>
+
+## Options
+
+### username (string)
+
+The github account username.
+
+	username: 'bjorn-aadnesgaard'
+
+### sortBy (string)
+
+Sort command for the returned projects. Supported commands are ___name___, ___watchers___.  
+_The default value is **name**._
+
+	sortBy: 'name'
+
+### outerClass (string)
+
+The outer element class name.  
+_The default value is **project-github**._
+
+	outerClass: 'project-github'
+
+### itemClass (string)
+
+The item element class name.  
+_The default value is **item**._
+
+	itemClass: 'item'
+
+### renderIcon (boolean)
+
+Renders the icon markup.  
+_The default value is **true**._
+
+	renderIcon: true
+
+### iconClass (string)
+
+The icon element class name.  
+_The default value is **icon**._
+
+	iconClass: 'icon'
+
+### renderButton (boolean)
+
+Renders the button markup.  
+_The default value is **true**._
+
+	renderButton: true
+
+### buttonClass (string)
+
+The button element class name.  
+_The default value is **btn**._
+
+	buttonClass: 'btn'
+
+### descriptionLimit (integer)
+
+Character limit for the description. Truncated text will display ___...___ suffix.  
+_The default value is **100**._
+	
+	descriptionLimit: 100
+
+### equalHeight (boolean)
+
+When true, each item will render with the height value set to that of the item with the greatest value. Set to true for consistent display when used in a column layout.  
+_The default value is **false**._
+	
+	equalHeight: false
+
+### equalHeightClass (string)
+
+The class added to the item element when equalHeight is true.  
+_The default value is **equal-height**._
+
+	equalHeightClass: 'equal-height'
+
+## Thanks
+
+The original source was found on Andrew Davey's blog at http://aboutcode.net/2010/11/11/list-github-projects-using-javascript.html.
---
+++ jquery.githubprojects.js
@@ -0,0 +1,92 @@
+﻿// https://github.com/bjorn-aadnesgaard/jquery.githubprojects
+// Original source code from http://aboutcode.net/2010/11/11/list-github-projects-using-javascript.html
+$.fn.githubprojects = function (options, callback) {
+	getGitHubUser = function (username, callback) {
+		$.getJSON("https://api.github.com/users/" + username + "/repos?callback=?", callback);
+	}
+
+	var settings = $.extend({
+		username: '',
+		sortBy: 'name',
+		outerClass: 'project-github',
+		itemClass: 'item',
+		renderIcon: true,
+		iconClass: 'icon', //
+		renderButton: true,
+		buttonClass: 'btn',
+		descriptionLimit: 100,
+		equalHeight: false,
+		equalHeightClass: 'equal-height'
+	}, options);
+
+	this.html('<div class="spin">Querying GitHub for repositories...</div>');
+
+	var target = this;
+	getGitHubUser(settings.username, function (response) {
+		var repos = response.data;
+
+		//Check if request limit reached
+		switch (response.meta.status) {
+			case 403:
+				target.html('<div class="alert alert-error">' + response.data.message + '</div>');
+
+				break;
+			default:
+				sort(repos, settings.sortBy);
+				var list = $('<div/>').addClass(settings.outerClass);
+				$(repos).each(function () {
+					if (!this.fork) {
+						var desc = this.description.substring(0, settings.maxDescription);
+						list.append(
+							'<div class="' + settings.itemClass + ((settings.equalHeight) ? " " + settings.equalHeightClass : "") + '">' +
+								'<div class="title">' +
+									'<h4>' +
+										((settings.renderIcon) ? '<i class="' + settings.iconClass + '"></i>' : "") +
+										'<a href="' + this.html_url + '">' + this.name + '</a>' +
+									'</h4>' +
+								'</div>' +
+								'<div class="description">' +
+									this.description.substring(0, settings.descriptionLimit) + ((this.description.length > desc.length) ? "..." : "") +
+								'</div>' +
+								((settings.renderButton) ? '<a href="' + this.html_url + '" class="' + settings.buttonClass + '">View Project</a>': "") +
+							'</div>'
+						);
+					}
+				});
+
+				//Assign the html output
+				target.html(list);
+
+				if (settings.equalHeight) {
+					//Ref http://stackoverflow.com/a/13227114/612113
+					//Set the max height of each span
+					var maxHeight = 0;
+					target.find('.' + settings.equalHeightClass).each(function () {
+						if ($(this).height() > maxHeight) {
+							maxHeight = $(this).height();
+						}
+					});
+					target.find('.' + settings.equalHeightClass).height(maxHeight);
+
+					if (typeof callback == 'function') { // make sure the callback is a function
+						callback.call(this); // brings the scope to the callback
+					}
+				}
+
+				break;
+		}
+	});
+
+	sort = function (repos, sortBy) {
+		repos.sort(function (a, b) {
+			switch (sortBy) {
+				case 'name':
+					return b.name - a.name;
+				case 'watchers':
+					return b.watchers - a.watchers;
+				default:
+					return b.name - a.name;
+			}
+		});
+	}
+}
